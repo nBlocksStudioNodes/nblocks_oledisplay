d95040bdb6708338df37e3e9c8c8f843a817ca75
nikos chalikias <nikos.chalikias@gmail.com>
2021-01-31 20:10:26 +0000

    Adafruit_GFX library and sceleton code

--- OLEDisplay.cpp
+++ OLEDisplay.cpp
@@ -8,35 +8,28 @@ nBlock_OLEDisplay::nBlock_OLEDisplay(PinName pinSDA, PinName pinSCL, int address
 		
 	_i2c.frequency(400000);
 		
-	// Store the I2C address
-	_address = address;
+//*******************************************
+	class I2CPreInit : public I2C
+	{
+	public:
+		I2CPreInit(PinName sda, PinName scl) : I2C(sda, scl)
+		{
+			frequency(400000);
+			start();
+		};
+	};
 	
-	// Store scale multiplier
-	_scale = scale;
-	
-	// Reset the flag, we are not requesting a read yet
-	_flagReadRequested = 0;
-	
-	// Configures to a minimum of 4 consecutive readings beyond threshold 
-	// to qualify as valid comparison
-	// And the specified comparator polarity
-	uint8_t config_value = __OLEDisplay_CONF_FAULTQUEUE_4 | ((comparatorPolarity & 0b00000001) << __OLEDisplay_CONF_POLARITY_Pos);
-	i2cWrite(__OLEDisplay_CONFIG, config_value, 1);
+	I2CPreInit gI2C(P0_7,P0_30);
+	Adafruit_SSD1306_I2c gOled(gI2C,P0_31);
 
-	// Configures low temperature threshold for comparator
-	uint16_t t_hyst = ((uint16_t)round(alarmMin * 2.0)) << 7;
-	i2cWrite(__OLEDisplay_ALARM_LOW, t_hyst, 2);
+	gOled.printf("%ux%u OLED Display\r\n", gOled.width(), gOled.height());
+//*************************************************
 	
-	// Configures high temperature threshold for comparator
-	uint16_t t_os = ((uint16_t)round(alarmMax * 2.0)) << 7;
-	i2cWrite(__OLEDisplay_ALARM_HIGH, t_os, 2);
-	
-}
+	}
 
 void nBlock_OLEDisplay::triggerInput(nBlocks_Message message) {
 	// inputNumber is ignored as we only have one input
 	// value is ignored as any value is considered a trigger for reading
-
 	// If we received ANY input, means we should read the sensor
 	_flagReadRequested = 1;
 
@@ -48,15 +41,11 @@ void nBlock_OLEDisplay::endFrame(void) {
 		// Clear the flag
 		_flagReadRequested = 0;
 		
-		// Perform the I2C read
-		
-		// Reads register and casts to float
-		float temperature_shifted = i2cRead(__OLEDisplay_TEMPERATURE, 2) * 1.0; // temperature is *signed* int
-		// Register value (temperature_shifted) is shifted 7 to left, 0.5 resolution
-		// That is, MSB is round Celsius
-		// temperature is Celsius as float
-		float temperature = temperature_shifted / 256; 
-		
+        //***************************
+		gOled.printf("%u\r",x);
+        gOled.display();
+		//*****************************
+
         output[0] = temperature*_scale;
 		available[0] = 1;
 				
@@ -64,51 +53,3 @@ void nBlock_OLEDisplay::endFrame(void) {
     return;
 }
 
-
-void nBlock_OLEDisplay::i2cWrite(uint8_t regAddress, uint16_t value, uint32_t num_bytes) {
-	char buf[3]; // [reg. number, MSB, LSB]
-	
-	buf[0] = regAddress;
-	
-	// 2-byte transmission
-	if (num_bytes == 2) {
-		buf[1] = (value >> 8) & 0xFF;
-		buf[2] = (value     ) & 0xFF;
-		_i2c.write(_address, buf, 3);
-	}
-	// 1-byte transmission
-	else {
-		buf[1] = (value     ) & 0xFF;
-		_i2c.write(_address, buf, 2);
-	}
-}
-
-uint16_t nBlock_OLEDisplay::i2cRead(uint8_t regAddress, uint32_t num_bytes) {
-	char buf[2]; // [reg. number, unused] or [MSB, LSB]
-
-	// Sets the register address
-	buf[0] = regAddress;
-	buf[1] = 0xFF;// dummy
-	
-	_i2c.write(_address, buf, 1);
-	
-	// Clears the buffer and result variable
-	buf[0] = 0;
-	buf[1] = 0;
-	uint16_t result = 0;
-	
-	// 2-byte reception
-	if (num_bytes == 2) {
-		_i2c.read(_address, buf, 2);
-		// Cast to uint16_t is important otherwise the value disappears on overflow
-		result = (((uint16_t)buf[0]) << 8) | buf[1];
-	}
-	// 1-byte reception
-	else {
-		_i2c.read(_address, buf, 1);
-		result = buf[0] & 0xFF;
-	}
-	
-	return result;
-	
-}
--- OLEDisplay.h
+++ OLEDisplay.h
@@ -4,91 +4,15 @@
 #include "mbed.h"
 #include "nworkbench.h"
 
-// OLEDisplay constants
-
-#define __OLEDisplay_ADDRESS_BASE = 0x90
-
-// All registers but config are 16-bit wide temperature values
-// where MSB is temperature in Celsius and only bit 7 of LSB
-// is used (represents 0.5 Celsius)
-#define __OLEDisplay_TEMPERATURE   0x00
-#define __OLEDisplay_CONFIG        0x01
-#define __OLEDisplay_ALARM_LOW     0x02
-#define __OLEDisplay_ALARM_HIGH    0x03
-
-#define __OLEDisplay_CONF_FAULTQUEUE_4  0b00010000
-#define __OLEDisplay_CONF_POLARITY_Pos  2
-
-
-/**
- *  \brief Driver to read data from OLEDisplay temperature
- *  sensors. It has a trigger input and 2 outputs: temperature and alarm
- *  The trigger is used to synchronise the output - as OLEDisplay has no internal
- *  event system for temperature
- */
 class nBlock_OLEDisplay: public nBlockSimpleNode<1> {
 public:
-    /**
-     *  \brief Constructor is invoked at node creation, and arguments
-     *  are configuration parameters assigned in the IDE
-     *  
-     *  \param [in] pinSDA Pin connected to the sensor's SDA line
-     *  \param [in] pinSCL Pin connected to the sensor's SCL line
-     *  \param [in] address I2C address
-     *  \param [in] alarmMin Lower threshold for temperature alarm
-     *  \param [in] alarmMax Upper threshold for temperature alarm
-     *  \param [in] comparatorPolarity Output value for the OS output when active
-     *  \param [in] scale Multiplier for temperature value as integer (scale=100 means 2.5C will be represented as 250)
-     *  
-     */
-    nBlock_OLEDisplay(PinName pinSDA, PinName pinSCL, int address, float alarmMin, float alarmMax, int comparatorPolarity, int scale);
-	
-    /**
-     *  \brief Called whenever a data packet is sent to this node in one
-     *  of the inputs. In this case, sets a flag indicating a read must be 
-     *  performed in the end of this frame, so data is available in the next.
-     *  Does not engage in bus communication yet.
-     *  
-     *  \param [in] inputNumber Number identifying the input. This node has only one.
-     *  \param [in] value The value sent to this node. In this node, any value triggers.
-     */
+
+    nBlock_OLEDisplay(PinName pinSDA, PinName pinSCL);
     void triggerInput(nBlocks_Message message);
-	
-    /**
-     *  \brief Called in the end of every frame.
-     *  In this case, reads the sensor if a read was requested and prepares
-     *  data in the output for the next frame.
-     */
     void endFrame(void);
-	
-	/**
-	 *  \brief Writes a 8-bit or 16-bit value into a register over I2C
-	 *  
-	 *  \param [in] regAddress Register address (range 0x00 - 0x03)
-	 *  \param [in] value Value to be written
-	 *  \param [in] num_bytes Number of bytes to send (1 or 2)
-	 */
-	void i2cWrite(uint8_t regAddress, uint16_t value, uint32_t num_bytes);
-	
-	/**
-	 *  \brief Reads a 16-bit register over I2C
-	 *  
-	 *  \param [in] regAddress Register address (range 0x00 - 0x03)
-	 *  \param [in] num_bytes Number of bytes to read (1 or 2)
-	 *  \return Register value as 16-bit integer
-	 *  
-	 */
-	uint16_t i2cRead(uint8_t regAddress, uint32_t num_bytes);
-	
-	
+
 private:
-	// I2C Object
 	I2C _i2c;
-	// I2C Address
-	int _address;
-	// Scale multiplier for output
-	int _scale;
-	// Flag indicating a read was requested
 	int _flagReadRequested;
 	
 };
