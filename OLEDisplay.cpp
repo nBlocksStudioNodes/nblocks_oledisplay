2e1875bb7cf2b7fa7e864bff6070675133f08e71
nikos chalikias <nikos.chalikias@gmail.com>
2021-02-01 15:56:50 +0000

    is working

--- OLEDisplay.cpp
+++ OLEDisplay.cpp
@@ -2,7 +2,7 @@
 
 nBlock_OLEDisplay::nBlock_OLEDisplay(PinName pinSDA, PinName pinSCL, PinName pinRST): _i2c(pinSDA, pinSCL) {		
 	_i2c.frequency(400000);
-	_i2c.start;
+	_i2c.start();
 	_gfx = new Adafruit_SSD1306_I2c(_i2c, pinRST) ;
 	_gfx->clearDisplay();			
 
@@ -12,9 +12,11 @@ nBlock_OLEDisplay::nBlock_OLEDisplay(PinName pinSDA, PinName pinSCL, PinName pin
 }
 
 void nBlock_OLEDisplay::triggerInput(nBlocks_Message message) {
-	uint32_t inputNumber = message.inputNumber;
-	char * value = message.stringValue;
-	strcpy(TextContent, value ); // Copy string to internal buffer
+	//uint32_t inputNumber = message.inputNumber;
+	//char * value = message.stringValue;
+	//strcpy(TextContent, value ); // Copy string to internal buffer
+
+    strcpy(TextContent, message.stringValue);
 	data_received = 1;
 }
 
@@ -22,6 +24,7 @@ void nBlock_OLEDisplay::endFrame(void) {
 	if (data_received ) {
 		data_received  = 0;
 		_gfx->printf(TextContent);
+        _gfx->display();
         _gfx->display();
 	}
 }
--- OLEDisplay.h
+++ OLEDisplay.h
@@ -3,17 +3,24 @@
 
 #include "mbed.h"
 #include "nworkbench.h"
+#include "Adafruit_SSD1306.h"
+#include "Adafruit_GFX.h"
 
 class nBlock_OLEDisplay: public nBlockSimpleNode<1> {
 public:
     nBlock_OLEDisplay(PinName pinSDA, PinName pinSCL, PinName pinRST);
     void triggerInput(nBlocks_Message message);
     void endFrame(void);
+    char TextContent[257]; // Up to 64x4 + NULL	
+    uint32_t inputNumber;
+
     
 private:
 	I2C _i2c;
     Adafruit_SSD1306_I2c * _gfx;
-	int data_received;	
+	int data_received;
+    	// Buffer to store the string
+	
 };
 
 #endif
